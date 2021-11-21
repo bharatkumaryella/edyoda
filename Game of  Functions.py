1c83c5ab69056918c76f39ef64d38796c8b44d59
Bharat kumar yella <89981877+bharatkumaryella@users.noreply.github.com>
2021-11-21 16:52:50 +0530

    Add files via upload

---
+++ Game of  Functions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#  Write a Python function to sum all the numbers in a list.
+
+
+
+# Sample List : (8, 2, 3, 0, 7)
+
+# Expected Output : 20
+
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total =total+ x
+    return total
+print(sum((8, 2, 3, 0, 7)))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
