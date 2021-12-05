7f63117e86d1acf8b1f056fab95d8e0fba981bdd
Bharat kumar yella <89981877+bharatkumaryella@users.noreply.github.com>
2021-12-05 10:51:06 +0530

    Add files via upload

---
+++ Play with Lambda.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+result = lambda x : x + 25
+print(result(10))
+
+
+# In[ ]:
+
+
+
+
